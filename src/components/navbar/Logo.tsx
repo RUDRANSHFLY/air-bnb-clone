4f14f952ad57c78159de67825fa926f734ca279f
RUDRANSHFLY <mayamayavir@gmail.com>
2024-08-16 18:08:16 +0530

    Route redireaction to HomePage

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Logo = () => {
+  const router = useRouter();
+
   return (
     <div className={"relative w-28 h-10"}>
       <Image
+        onClick={() => router.push("/")}
         className={"hidden md:block cursor-pointer absolute"}
         fill
         priority
