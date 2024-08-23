60c6910f396542b50d25daaa4360504a0962df48
RUDRANSHFLY <mayamayavir@gmail.com>
2024-08-23 09:48:29 +0530

    Rent Modal Store Add

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,16 @@ import { create } from "zustand";
 type Store = {
   signInSheetOpen: boolean;
   signUpSheetOpen: boolean;
+  rentModalSheetOpen: boolean;
   setSignInSheetOpen: () => void;
   setSignUpSheetOpen: () => void;
+  setRentModalSheetOpen: () => void;
 };
 
 export const useStore = create<Store>()((set) => ({
   signInSheetOpen: false,
   signUpSheetOpen: false,
+  rentModalSheetOpen: false,
   setSignInSheetOpen: () =>
     set((state) => ({
       signInSheetOpen: !state.signInSheetOpen,
@@ -17,5 +20,9 @@ export const useStore = create<Store>()((set) => ({
   setSignUpSheetOpen: () =>
     set((state) => ({
       signUpSheetOpen: !state.signUpSheetOpen,
+    })),
+  setRentModalSheetOpen: () =>
+    set((state) => ({
+      rentModalSheetOpen: !state.rentModalSheetOpen,
     })),
 }));
