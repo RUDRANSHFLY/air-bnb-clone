0ab44e2cb9a957342c5f5caf1098148a86fcfa3f
RUDRANSHFLY <mayamayavir@gmail.com>
2024-08-28 10:13:02 +0530

    Modal Comp Done

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import React, { ReactElement, useState, useEffect, useCallback } from "react";
+import { Button } from "../ui/button";
+import { CircleX, CrossIcon } from "lucide-react";
+import ButtonHelper from "../helper/ButtonHelper";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -12,14 +16,14 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal = ({
   actionLabel,
   onCLose,
   onSubmit,
-  secondaryLabel,
+  secondaryActionLabel,
   body,
   disabled,
   footer,
@@ -40,9 +44,8 @@ const Modal = ({
 
     setShowModal(false);
     setTimeout(() => {
-      7;
       onCLose();
-    }, 3000);
+    }, 200);
   }, [disabled, onCLose]);
 
   const handleSubmit = useCallback(() => {
@@ -60,14 +63,76 @@ const Modal = ({
     secondaryAction();
   }, [disabled, secondaryAction]);
 
-  if (!open) {
+  if (!isOpen) {
     return null;
   }
 
   return (
-    <>
-      <div></div>
-    </>
+    <div
+      className={
+        "justify-center items-center flex overflow-x-hidden overflow-y-scroll scrollbar-hide fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
+      }
+    >
+      <div
+        className={
+          "relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5  mx-auto h-full lg:h-auto md:h-auto"
+        }
+      >
+        {/*content*/}
+        <div
+          className={cn(
+            `duration-200 h-full ${
+              showModal
+                ? "translate-y-0 opacity-100"
+                : "translate-y-full opacity-0"
+            }`
+          )}
+        >
+          <div
+            className={
+              "h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+            }
+          >
+            {/*header*/}
+            <div
+              className={
+                "relative flex justify-center items-center p-6 rounded-t border-b-[1px]"
+              }
+            >
+              <Button
+                variant={"link"}
+                onClick={handleClose}
+                className={"absolute right-9"}
+              >
+                <CircleX size={24} />
+              </Button>
+              <div className={"text-lg font-semibold "}>{title}</div>
+            </div>
+            {/*body*/}
+            <div className={"relative flex-auto"}>{body}</div>
+            {/*footer*/}
+            <div className={"flex flex-col gap-2 p-6"}>
+              <div className={"flex flex-row items-center gap-4 w-full"}>
+                {secondaryAction && secondaryActionLabel && (
+                  <ButtonHelper
+                    outline
+                    label={secondaryActionLabel}
+                    disabled={disabled}
+                    onClick={handleSecondaryAction}
+                  />
+                )}
+
+                <ButtonHelper
+                  label={actionLabel}
+                  disabled={disabled}
+                  onClick={handleSubmit}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
