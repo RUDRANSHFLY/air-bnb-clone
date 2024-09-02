20028d9bd245332196fb51730855f616b4254d33
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-02 21:15:19 +0530

    Reservation Add

--- src/app/listings/[listingID]/page.tsx
+++ src/app/listings/[listingID]/page.tsx
@@ -1,9 +1,11 @@
 import getCurrentUser from "@/actions/getCurrentUser";
 import getListingByID from "@/actions/getListingbyID";
+import getReservations from "@/actions/getReservations";
 import ListingClient from "@/components/card/ListingClient";
 import NavBar from "@/components/navbar/NavBar";
 import EmptyState from "@/components/rent/EmptyState";
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 interface IParams {
   listingID?: string;
@@ -12,6 +14,7 @@ interface IParams {
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingByID(params);
   const currentUser = await getCurrentUser();
+  const reservation = await getReservations(params);
 
   if (!listing) {
     return (
@@ -23,6 +26,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <div>
+      <Toaster />
       <NavBar currentUser={currentUser} />
       <div className={"pt-28 pb-20"}>
         <ListingClient listing={listing} currentUser={currentUser} />
