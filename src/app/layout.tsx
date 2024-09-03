2436077be19bd48ec9bdacdf135b58e589feb27e
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-03 13:20:37 +0530

    NavBar &  Auth & Rent Models Add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter, Aboreto } from "next/font/google";
 import "./globals.css";
+import getCurrentUser from "@/actions/getCurrentUser";
+import NavBar from "@/components/navbar/NavBar";
+import SignInComp from "@/components/auth/SignInComp";
+import SignUpComp from "@/components/auth/SignUpComp";
+import Rent from "@/components/rent/Rent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,14 +16,23 @@ export const metadata: Metadata = {
   description: "an rental company Clone like AirBnB Clone",
 };
 
-export default function RootLayout({
+export const dynamic = "force-dynamic";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const sessionCurrentUser = await getCurrentUser();
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <NavBar currentUser={sessionCurrentUser} />
+        <SignInComp />
+        <SignUpComp />
+        <Rent />
+        {children}
+      </body>
     </html>
   );
 }
