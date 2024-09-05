0f2e5ef49ff87ced38895d530f4ef1735f86fcd4
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-05 09:05:15 +0530

    GetFavoruiteLsitings Add

---
+++ src/actions/getFavouriteListings.ts
@@ -0,0 +1,28 @@
+import client from "@/libs/prsimaDB";
+
+import getCurrentUser from "./getCurrentUser";
+
+export default async function getFavouriteLisitings() {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+      return [];
+    }
+    const favorites = await client.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoriteIds || [])],
+        },
+      },
+    });
+
+    const safeFavorites = favorites.map((favorite) => ({
+      ...favorite,
+      createdAt: favorite.createdAt.toISOString(),
+    }));
+
+    return safeFavorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
