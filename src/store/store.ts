564183d14b5242ba081332575060ccb00fb37fd3
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-05 09:10:04 +0530

    Filter model open and setOpen Function Add

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,15 +4,18 @@ type Store = {
   signInSheetOpen: boolean;
   signUpSheetOpen: boolean;
   rentModalSheetOpen: boolean;
+  filterSheetOpen: boolean;
   setSignInSheetOpen: () => void;
   setSignUpSheetOpen: () => void;
   setRentModalSheetOpen: () => void;
+  setFilterSheetOpen: () => void;
 };
 
 export const useStore = create<Store>()((set) => ({
   signInSheetOpen: false,
   signUpSheetOpen: false,
   rentModalSheetOpen: false,
+  filterSheetOpen: false,
   setSignInSheetOpen: () =>
     set((state) => ({
       signInSheetOpen: !state.signInSheetOpen,
@@ -24,5 +27,9 @@ export const useStore = create<Store>()((set) => ({
   setRentModalSheetOpen: () =>
     set((state) => ({
       rentModalSheetOpen: !state.rentModalSheetOpen,
+    })),
+  setFilterSheetOpen: () =>
+    set((state) => ({
+      filterSheetOpen: !state.filterSheetOpen,
     })),
 }));
