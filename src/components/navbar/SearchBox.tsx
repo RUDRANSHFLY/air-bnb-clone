dfe9988982dd9ba0f31723bbb12cb1ac39b108c0
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 08:57:16 +0530

    Dynamic Value Assign in Search Box

--- src/components/navbar/SearchBox.tsx
+++ src/components/navbar/SearchBox.tsx
@@ -1,11 +1,52 @@
 "use client";
 
+import useCountries from "@/actions/getCountires";
 import { useStore } from "@/store/store";
+import { differenceInDays } from "date-fns";
 import { Search } from "lucide-react";
-import React from "react";
+import { useSearchParams } from "next/navigation";
+import React, { useMemo } from "react";
 
 const SearchBox = () => {
   const setFilterModal = useStore((state) => state.setFilterSheetOpen);
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.Label;
+    }
+
+    return "AnyWhere";
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+
+      let diff = differenceInDays(end, start);
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+    return "Add Guests";
+  }, [guestCount]);
 
   return (
     <div
@@ -16,14 +57,14 @@ const SearchBox = () => {
     >
       <div className={"flex flex-row items-center justify-between"}>
         <div className={"text-sm font-semibold px-6"}>
-          <p>AnyWhere</p>
+          <p>{locationLabel}</p>
         </div>
         <div
           className={
             "hidden text-sm sm:block font-semibold px-6 border-x-[1px] flex-1 text-center"
           }
         >
-          <p>Any-week</p>
+          <p>{durationLabel}</p>
         </div>
         <div
           className={
@@ -31,7 +72,7 @@ const SearchBox = () => {
           }
         >
           <div className={"hidden sm:block"}>
-            <p>Add Guests</p>
+            <p>{guestLabel}</p>
           </div>
           <div className={"p-2 bg-rose-500 rounded-full text-white"}>
             <Search className="w-5 h-5" />
