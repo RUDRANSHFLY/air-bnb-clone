2b90608b71e53b42f446b6b1e462e1fb6fff6786
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 08:58:11 +0530

    loadin Page Done

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from "@/components/helper/Loading";
+import React from "react";
+
+const loader = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+};
+
+export default loader;
